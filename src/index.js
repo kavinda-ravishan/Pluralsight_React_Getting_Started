425f6295826a97e7d772f99e1966e95a2edbc310
kavinda ravishan <kvnravishan@outlook.com>
2021-04-02 22:10:37 +0530

    component reusability

--- src/index.js
+++ src/index.js
@@ -14,7 +14,9 @@ ReactDOM.render(
 */
 
 function Button(props) {
-  return <button onClick={props.onClickFunction}>+1</button>;
+  const handleClick = () => props.onClickFunction(props.increment);
+
+  return <button onClick={handleClick}>+{props.increment}</button>;
 }
 
 function Display(props) {
@@ -23,10 +25,15 @@ function Display(props) {
 
 function App() {
   const [counter, setCounter] = useState(0);
-  const incrementCounter = () => setCounter(counter + 1);
+  const incrementCounter = (incrementValue) =>
+    setCounter(counter + incrementValue);
+
   return (
     <div>
-      <Button onClickFunction={incrementCounter} />
+      <Button onClickFunction={incrementCounter} increment={1} />
+      <Button onClickFunction={incrementCounter} increment={5} />
+      <Button onClickFunction={incrementCounter} increment={10} />
+      <Button onClickFunction={incrementCounter} increment={100} />
       <Display message={counter} />
     </div>
   );
