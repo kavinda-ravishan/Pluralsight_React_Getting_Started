8d769b57eec9629f8a7ef74beb3d48bb9d08871b
kavinda ravishan <kvnravishan@outlook.com>
2021-04-02 22:00:34 +0530

    Data flow part 2

--- src/index.js
+++ src/index.js
@@ -13,35 +13,26 @@ ReactDOM.render(
 );
 */
 
-function Button() {
-  const [counter, setCounter] = useState(0); //init value 0 //Hook
-  const handleClick = () => setCounter(counter + 1);
-
-  return <button onClick={handleClick}>{counter}</button>;
+function Button(props) {
+  return <button onClick={props.onClickFunction}>+1</button>;
 }
 
-function Display() {
-  return <div>...</div>;
+function Display(props) {
+  return <div>{props.message}</div>;
 }
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const incrementCounter = () => setCounter(counter + 1);
   return (
     <div>
-      <Button />
-      <Display />
+      <Button onClickFunction={incrementCounter} />
+      <Display message={counter} />
     </div>
   );
 }
 
-ReactDOM.render(
-  //[<Button />, <Display />],
-  //or
-  //<><Button /><Display /></>,//Same as React.Fragment
-  //or
-  //<div><Button /><Display /></div>,
-  <App />,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
