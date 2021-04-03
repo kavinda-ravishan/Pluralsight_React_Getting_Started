77bbd1fa7feff551dfb72f49a7e3ded548f5fd22
kavinda ravishan <kvnravishan@outlook.com>
2021-04-03 14:10:10 +0530

    the github card app init

--- src/index.js
+++ src/index.js
@@ -1,53 +1,13 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-//import App from './App';
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-/*
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App title="The GitHub Card App" />,
+  document.getElementById("root")
 );
-*/
-
-function Button(props) {
-  const handleClick = () => props.onClickFunction(props.increment);
-
-  return <button onClick={handleClick}>+{props.increment}</button>;
-}
-
-function Display(props) {
-  return <div>{props.message}</div>;
-}
-
-function DisplayTime(props) {
-  return <pre>{props.time}</pre>;
-}
-
-function App() {
-  const [counter, setCounter] = useState(0);
-  const incrementCounter = (incrementValue) =>
-    setCounter(counter + incrementValue);
-
-  const [time, setTime] = useState(new Date().toLocaleTimeString());
-  setInterval(() => setTime(new Date().toLocaleTimeString()), 1000);
-
-  return (
-    <div>
-      <Button onClickFunction={incrementCounter} increment={1} />
-      <Button onClickFunction={incrementCounter} increment={5} />
-      <Button onClickFunction={incrementCounter} increment={10} />
-      <Button onClickFunction={incrementCounter} increment={100} />
-      <Display message={counter} />
-      <DisplayTime time={time} />
-    </div>
-  );
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
