bbd8a97d76f102c496140ea207b985873067e3fd
kavinda ravishan <kvnravishan@outlook.com>
2021-04-03 14:15:31 +0530

    the github card app func to class

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import React from "react";
 import "./App.css";
 
+/*
 const App = ({ title }) => {
   return <div className="header">{title}</div>;
 };
+*/
+
+class App extends React.Component {
+  render() {
+    return <div className="header">{this.props.title}</div>;
+  }
+}
 
 export default App;
