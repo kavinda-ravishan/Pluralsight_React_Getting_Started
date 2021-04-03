420c66d05d255e48a4bbff611b304ad5e088592b
kavinda ravishan <kvnravishan@outlook.com>
2021-04-03 14:33:23 +0530

    the github card app 2.2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from "react";
 import "./App.css";
 
-/*
-const App = ({ title }) => {
-  return <div className="header">{title}</div>;
-};
-*/
+class Card extends React.Component {
+  render() {
+    return (
+      <div className="github-profile">
+        <img src="https://via.placeholder.com/75"></img>
+        <div className="info">
+          <div className="name">Name here...</div>
+          <div className="company">Company here...</div>
+        </div>
+      </div>
+    );
+  }
+}
 
 class App extends React.Component {
   render() {
-    return <div className="header">{this.props.title}</div>;
+    return (
+      <div>
+        <div className="header">{this.props.title}</div>
+        <Card />
+      </div>
+    );
   }
 }
 
